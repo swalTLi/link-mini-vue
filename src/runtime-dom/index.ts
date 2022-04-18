12cdadb7f0dcbfd311e35b47d26a99ebb08fe682
origin <2586463185@qq.com>
2022-04-18 10:44:53 +0800

    更新 element 的props

--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -1,34 +1,35 @@
-import { createRenderer } from '../runtime-core'
+import { createRenderer } from "../runtime-core";
 
 function createElement(type) {
-  return document.createElement(type)
+  return document.createElement(type);
 }
 
-function patchProp(el, key, val) {
-  const isOn = (key: string) => /^on[A-Z]/.test(key)
+function patchProp(el, key, prevVal, nextVal) {
+  const isOn = (key: string) => /^on[A-Z]/.test(key);
   if (isOn(key)) {
-    // const EventTag = key.substring(0, 2)
-    // if (EventTag === "on") {
-    const EventName = key.substring(2).toLowerCase()
-    el.addEventListener(EventName, val)
-    // console.log(EventTag, EventName);
+    const event = key.slice(2).toLowerCase();
+    el.addEventListener(event, nextVal);
   } else {
-    el.setAttribute(key, val)
+    if (nextVal === undefined || nextVal === null) {
+      el.removeAttribute(key);
+    } else {
+      el.setAttribute(key, nextVal);
+    }
   }
 }
 
 function insert(el, parent) {
-  parent.append(el)
+  parent.append(el);
 }
 
 const renderer: any = createRenderer({
   createElement,
   patchProp,
-  insert
-})
+  insert,
+});
 
 export function createApp(...args) {
-  return renderer.createApp(...args)
+  return renderer.createApp(...args);
 }
 
-export * from "../runtime-core"
+export * from "../runtime-core";
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,7 @@
 export const extend = Object.assign;
 
+export const EMPTY_OBJ = {};
+
 export const isObject = (value) => {
   return value !== null && typeof value === "object";
 };
@@ -7,19 +9,20 @@ export const isObject = (value) => {
 export const hasChanged = (val, newValue) => {
   return !Object.is(val, newValue);
 };
-export const hasOwn = (val, key) => {
-  return Object.prototype.hasOwnProperty.call(val, key)
-}
+
+export const hasOwn = (val, key) =>
+  Object.prototype.hasOwnProperty.call(val, key);
 
 export const camelize = (str: string) => {
   return str.replace(/-(\w)/g, (_, c: string) => {
-    return c ? c.toUpperCase() : " "
-  })
-}
+    return c ? c.toUpperCase() : "";
+  });
+};
 
-export const capotalize = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
+const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 export const toHandlerKey = (str: string) => {
-  return str ? "on" + capotalize(str) : ""
-}
+  return str ? "on" + capitalize(str) : "";
+};
